7b4b7a9794aa909779ecdeaa9ede93a92f62b46b
TiMESPLiNTER <github@timesplinter.ch>
2020-03-28 13:06:06 +0100

    - Add webfinger integration test - Improve test failure description for request comparison

---
+++ tests/Integration/Controller/Webfinger/WebfingerControllerTest.php
@@ -0,0 +1,75 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Integration\Controller\Webfinger;
+
+use Mitra\Tests\Integration\CreateUserTrait;
+use Mitra\Tests\Integration\IntegrationTestCase;
+
+/**
+ * @group Integration
+ */
+final class WebfingerControllerTest extends IntegrationTestCase
+{
+    use CreateUserTrait;
+
+    public function testWebfingerReturnsResourceInformation(): void
+    {
+        $user = $this->createUser();
+        $resource = sprintf('acct:%s@localhost', $user->getPreferredUsername());
+
+        $request = $this->createRequest('GET', sprintf(
+            '/.well-known/webfinger?resource=%s',
+            $resource
+        ));
+        $response = $this->executeRequest($request);
+
+        self::assertStatusCode(200, $response);
+
+        $actualPayload = json_decode((string) $response->getBody(), true);
+        $expectedPayload = [
+            'subject' => $resource,
+            'aliases' => [],
+            'links' => [
+                [
+                    'rel' => 'self',
+                    'type' => 'application/activity+json',
+                    'href' => sprintf('http://localhost/users/%s', $user->getPreferredUsername()),
+                ],
+            ],
+        ];
+
+        self::assertEquals($expectedPayload, $actualPayload);
+    }
+
+    public function testWebfingerReturnsBadRequestIfResourceQueryParamIsMalformed(): void
+    {
+        $request = $this->createRequest('GET', sprintf(
+            '/.well-known/webfinger?resource=ac:%s@example.org',
+            uniqid()
+        ));
+        $response = $this->executeRequest($request);
+
+        self::assertStatusCode(400, $response);
+    }
+
+    public function testWebfingerReturnsBadRequestIfResourceIdIsMalformed(): void
+    {
+        $request = $this->createRequest('GET', '/.well-known/webfinger?resource=acct:foo');
+        $response = $this->executeRequest($request);
+
+        self::assertStatusCode(400, $response);
+    }
+
+    public function testWebfingerReturnsNotFoundForNotExistingUser(): void
+    {
+        $request = $this->createRequest('GET', sprintf(
+            '/.well-known/webfinger?resource=acct:%s@example.org',
+            uniqid()
+        ));
+        $response = $this->executeRequest($request);
+
+        self::assertStatusCode(404, $response);
+    }
+}
