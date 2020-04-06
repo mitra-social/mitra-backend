ea8a018605ec782ae5bbad4f0e4fc8f8ff69a9bd
TiMESPLiNTER <github@timesplinter.ch>
2020-04-06 22:58:30 +0200

    Improve HttpKeyStore

--- src/Http/Signature/HttpKeyStore.php
+++ src/Http/Signature/HttpKeyStore.php
@@ -5,7 +5,10 @@ declare(strict_types=1);
 namespace Mitra\Http\Signature;
 
 use HttpSignatures\Key;
+use HttpSignatures\KeyException;
+use HttpSignatures\KeyStoreException;
 use HttpSignatures\KeyStoreInterface;
+use Psr\Http\Client\ClientExceptionInterface;
 use Psr\Http\Client\ClientInterface;
 use Psr\Http\Message\RequestFactoryInterface;
 
@@ -28,31 +31,59 @@ final class HttpKeyStore implements KeyStoreInterface
     }
 
     /**
-     * @inheritDoc
+     * @param string $keyId
+     * @return Key
+     * @throws KeyStoreException
      */
     public function fetch($keyId)
     {
-        $response = $this->httpClient->sendRequest(
-            $this->requestFactory->createRequest('GET', $keyId)
-                ->withHeader('Accept', 'application/activity+json, application/json')
-        );
+        try {
+            $response = $this->httpClient->sendRequest(
+                $this->requestFactory->createRequest('GET', $keyId)
+                    ->withHeader('Accept', 'application/activity+json, application/json')
+            );
+        } catch (ClientExceptionInterface $e) {
+            throw new KeyStoreException(sprintf(
+                'Could not fetch public key for key id `%s`: request failed',
+                $keyId
+            ), 1, $e);
+        }
 
         if ($response->getStatusCode() > 399) {
-            throw new \RuntimeException(sprintf(
+            throw new KeyStoreException(sprintf(
                 'Could not fetch public key for key id `%s`: remote server answered with HTTP status code `%d`',
                 $keyId,
                 $response->getStatusCode()
-            ));
+            ), 2);
         }
 
-        $decodedResponse = json_decode((string) $response->getBody(), true, 512, JSON_THROW_ON_ERROR);
+        $responseBody = (string) $response->getBody();
 
-        if (!isset($decodedResponse['publicKey']['publicKeyPem'])) {
-            throw new \RuntimeException(
-                'Could not find the public key within the response body under /publicKey.publicKeyPem'
+        try {
+            $decodedResponse = json_decode($responseBody, true, 512, JSON_THROW_ON_ERROR);
+        } catch (\JsonException $e) {
+            throw new KeyStoreException(
+                sprintf('Could not parse response: %s (body: %s)', $e->getMessage(), $responseBody),
+                3,
+                $e
             );
         }
 
-        return new Key($keyId, $decodedResponse['publicKey']['publicKeyPem']);
+        if (!isset($decodedResponse['publicKey']['publicKeyPem'])) {
+            throw new KeyStoreException(sprintf(
+                'Could not find the public key within the response body under $publicKey.publicKeyPem (body: %s)',
+                $responseBody
+            ), 4);
+        }
+
+        try {
+            return new Key($keyId, $decodedResponse['publicKey']['publicKeyPem']);
+        } catch (KeyException $e) {
+            throw new KeyStoreException(
+                sprintf('Could not create key from $publicKey.publicKeyPem: %s', $e->getMessage()),
+                5,
+                $e
+            );
+        }
     }
 }
