161948ed59f3037ef6d7a9bd2e7b4de4198458b0
TiMESPLiNTER <github@timesplinter.ch>
2020-06-05 18:48:12 +0200

    feat(): Fix validation

--- src/Mapping/Validation/ActivityPub/ObjectDtoValidationMapping.php
+++ src/Mapping/Validation/ActivityPub/ObjectDtoValidationMapping.php
@@ -113,7 +113,7 @@ class ObjectDtoValidationMapping implements ValidationMappingInterface
                 new Type('string'),
                 new Regex('~^[-\w.]+/[+-\w.]+$~'),
             ])
-            ->addPropertyConstraints('mediaType', [
+            ->addPropertyConstraints('duration', [
                 new Type('string'),
                 // P[JY][MM][WW][TD][T[hH][mM][s[.f]S]]
                 new Regex('/^P(?!$)(\d+Y)?(\d+M)?(\d+W)?(\d+D)?(T(?=\d)(\d+H)?(\d+M)?(\d+(\.\d+)?S)?)?$/'),
