0791199a4a49076c49bf0e0bb163798b118b78ef
TiMESPLiNTER <github@timesplinter.ch>
2020-06-06 10:06:51 +0200

    feat(): Add test for not handling content twice

--- tests/Integration/Controller/ActivityPub/InboxWriteControllerTest.php
+++ tests/Integration/Controller/ActivityPub/InboxWriteControllerTest.php
@@ -138,14 +138,14 @@ final class InboxWriteControllerTest extends IntegrationTestCase
             ->withHeader('Content-Type', 'application/activity+json');
 
         $objectAndToResponse1->getBody()->write(
-            '{"type": "OrderedCollection", "totalItems": 1, "first": "'.$externalCollectionFirstPage.'"}'
+            '{"type": "OrderedCollection", "totalItems": 1, "first": "' . $externalCollectionFirstPage . '"}'
         );
 
         $objectAndToResponse2 = self::$responseFactory->createResponse(200)
             ->withHeader('Content-Type', 'application/activity+json');
 
         $objectAndToResponse2->getBody()->write(
-            '{"type": "OrderedCollectionPage", "totalItems": 1, "orderedItems": ["'.$toUserExternalId.'"]}'
+            '{"type": "OrderedCollectionPage", "totalItems": 1, "orderedItems": ["' . $toUserExternalId . '"]}'
         );
 
         $objectAndToResponse3 = self::$responseFactory->createResponse(404);
@@ -197,5 +197,73 @@ final class InboxWriteControllerTest extends IntegrationTestCase
 
         self::assertCount(1, $userContent);
         self::assertEquals($userContent[0]->getContent()->getExternalId(), $dto->id);
+    }
+
+
+    public function testDonNotHandleAlreadyHandledContentTwice(): void
+    {
+        /** @var UriGenerator $uriGenerator */
+        $uriGenerator = $this->getContainer()->get(UriGenerator::class);
+
+        $toUser = $this->createInternalUser();
+        $ccUser = $this->createInternalUser();
+        $toUserExternalId = $uriGenerator->fullUrlFor('user-read', ['username' => $toUser->getUsername()]);
+        $ccUserExternalId = $uriGenerator->fullUrlFor('user-read', ['username' => $ccUser->getUsername()]);
+        $bccUser = $this->createInternalUser();
+        $btoUser = $this->createInternalUser();
+        $externalUser = $this->createExternalUser();
+
+        foreach ([$toUser, $ccUser, $bccUser, $btoUser] as $user) {
+            /** @var InternalUser $user */
+            $this->createSubscription($user->getActor(), $externalUser->getActor());
+        }
+
+        $dtoContent = 'This is a note.';
+
+        $dto = new CreateDto();
+        $dto->id = sprintf('https://example.com/user/%s/post/123456', $externalUser->getPreferredUsername());
+        $dto->actor = $externalUser->getExternalId();
+        $dto->object = new NoteDto();
+        $dto->object->content = $dtoContent;
+        $dto->to = [
+            $toUserExternalId,
+        ];
+        $dto->cc = [
+            $ccUserExternalId,
+        ];
+        $dto->bto = [
+            $uriGenerator->fullUrlFor('user-read', ['username' => $btoUser->getUsername()]),
+        ];
+        $dto->bcc = [
+            $uriGenerator->fullUrlFor('user-read', ['username' => $bccUser->getUsername()]),
+        ];
+
+        /** @var EncoderInterface $encoder */
+        $encoder = $this->getContainer()->get(EncoderInterface::class);
+        /** @var NormalizerInterface $normalizer */
+        $normalizer = $this->getContainer()->get(NormalizerInterface::class);
+
+        $payload = $encoder->encode($normalizer->normalize($dto), 'application/json');
+
+        $request = $this->createRequest('POST', sprintf('/user/%s/inbox', $toUser->getUsername()), $payload);
+        $response = $this->executeRequest($request);
+
+        self::assertStatusCode(201, $response);
+
+        /** @var ActivityStreamContentAssignmentRepository $contentAssignmentRepository */
+        $contentAssignmentRepository = $this->getContainer()->get(ActivityStreamContentAssignmentRepository::class);
+
+        foreach ([$toUser, $ccUser, $btoUser, $bccUser] as $user) {
+            /** @var ActivityStreamContentAssignment[] $userContent */
+            $userContent = $contentAssignmentRepository->findContentForActor($user->getActor(), null, null);
+
+            self::assertCount(1, $userContent);
+            self::assertEquals($userContent[0]->getContent()->getExternalId(), $dto->id);
+        }
+
+        $request = $this->createRequest('POST', sprintf('/user/%s/inbox', $toUser->getUsername()), $payload);
+        $response = $this->executeRequest($request);
+
+        self::assertStatusCode(201, $response);
     }
 }
