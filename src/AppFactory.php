be2d4352cebd397677a7dd23a1f33ba047ded071
TiMESPLiNTER <github@timesplinter.ch>
2020-06-18 13:02:28 +0200

    fix(): Revert middleware order

--- src/AppFactory.php
+++ src/AppFactory.php
@@ -39,8 +39,6 @@ final class AppFactory
         /** @var ContainerInterface $container */
         $container = $app->getContainer();
 
-        $app->add(ValidateHttpSignatureMiddleware::class);
-        $app->add(AcceptAndContentTypeMiddleware::class);
         $app->add(RequestCycleCleanupMiddleware::class);
 
         // Needs to be last middleware to handle all the errors
@@ -52,8 +50,15 @@ final class AppFactory
         );
         $errorMiddleware->setErrorHandler(HttpException::class, HttpErrorHandler::class, true);
 
-        $app->group('', new ApiPublicRouterProvider());
-        $app->group('', new ApiPrivateRouteProvider())->add(JwtAuthentication::class);
+        // API group
+        $app->group('', function () use ($app): void {
+            $app->group('', new ApiPublicRouterProvider());
+            $app->group('', new ApiPrivateRouteProvider())->add(JwtAuthentication::class);
+        })
+            ->add(ValidateHttpSignatureMiddleware::class)
+            ->add(AcceptAndContentTypeMiddleware::class);
+
+        $app->group('', new MediaPublicRouteProvider());
 
         return $app;
     }
